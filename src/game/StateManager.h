956b9d4fb0bee7a41e0197e28fad71b917ff6af1
mfitzek <m4rfitzek@gmail.com>
2022-04-25 14:22:41 +0200

    state manager

--- src/game/StateManager.h
+++ src/game/StateManager.h
@@ -1,15 +1,35 @@
+#pragma once
+
+
+#include "entity.h"
+#include "player.h"
+#include "window.h"
 
 
 
+#include <vector>
+#include <memory>
+
+using std::vector;
+using std::shared_ptr;
 
 class StateManager {
 
     private:
+
+        vector<shared_ptr<Entity>> entities;
+        shared_ptr<Player> player;
+
         static StateManager s_instance;
         StateManager();
 
     public:
 
+        void AddEntity(shared_ptr<Entity> entity);
+
+        void Update(float delta);
+        void Tick();
+        void Render();
         static StateManager& Get();
 };
 
