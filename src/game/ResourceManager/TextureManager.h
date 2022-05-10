a7a95e1545990a2c307629b7f6a6073896ed6377
mfitzek <m4rfitzek@gmail.com>
2022-05-10 12:18:36 +0200

    Texture manager

---
+++ src/game/ResourceManager/TextureManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include <unordered_map>
+#include <string
+
+class TextureManager {
+
+    private:
+        std::unordered_map<std::string, SDL_Surface*>
+
+};
